c47c73c2ab34d433e7a9dea963a13bb3f3793b42
tfotsonono <fotsonono2003@yahoo.fr>
2015-01-05 01:24:21 +0100

    ajout du fichier TestUser

---
+++ TestUser.java
@@ -0,0 +1,24 @@
+
+package test;
+import java.util.Date;
+import technique.*;
+/**
+ *
+ * @author Ibtihel
+ */
+public class TestUser
+{
+
+    
+    
+    public static void main(String[] args) throws Exception {
+
+        //UtilisateurDAO users=new UtilisateurDAO();
+        //users.afficherUsers();
+        String s = "JavaTest";
+        int a=s.indexOf("java");
+        System.out.println("a:"+a);
+        Date d=new Date("");
+        java.util.Date aDate = df.parse("22/04/2011");    }
+
+}
